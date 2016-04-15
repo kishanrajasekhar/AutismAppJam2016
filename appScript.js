da699b24d58eaf0e394da07d54f99ba8af34fe47
Kishan Rajasekhar <krajasek@uci.edu>
2016-04-14 23:08:08 -0700

    Used html5 local storage to store journal entries

--- appScript.js
+++ appScript.js
@@ -11,5 +11,20 @@ function addEntry(){
 }
 
 function entrySubmission(){
-	
+	var entry = $("#Journal").val();
+	var lesson = $("#Lesson").val();
+	if(entry.trim()=="" || lesson.trim()=="")
+		return;
+	$("#Journal").val(""); //set it back to empty
+	$("#Lesson").val(""); //set it back to empty
+	var timestamp = getTimeStamp();
+	console.log(timestamp);
+	localStorage.setItem(getTimeStamp(), lesson);
+}
+
+function getTimeStamp(){
+	var d = new Date();
+	var date = d.toDateString();
+	var time = d.getHours() + ":" + d.getMinutes();
+	return timestamp = date + " " + time;	
 }
